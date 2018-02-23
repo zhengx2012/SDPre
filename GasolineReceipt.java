41e619b468d84f877839cebd486285ac9772a2aa
Xian Zheng <xzheng2112@gmail.com>
2018-02-23 13:01:27 -0700

    Denver Prices

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -4,12 +4,12 @@ public class GasolineReceipt {
     System.out.println( "|                        |" );
     System.out.println( "|      CORNER STORE      |" );
     System.out.println( "|                        |" );
-    System.out.println( "| 2015-03-29  04:38PM    |" );
+    System.out.println( "| 2018-02-23  04:38PM    |" );
     System.out.println( "|                        |" );
     System.out.println( "| Gallons:       10:870  |" );
-    System.out.println( "| Price/gallon: $ 2.089  |" );
+    System.out.println( "| Price/gallon: $ 2.389  |" );
     System.out.println( "|                        |" );
-    System.out.println( "| Fuel total:   $ 22.71  |" );
+    System.out.println( "| Fuel total:   $ 25.97  |" );
     System.out.println( "|                        |" );
     System.out.println( "+------------------------+" );
   }
