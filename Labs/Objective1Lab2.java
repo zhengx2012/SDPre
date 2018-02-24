64846e09145673c131ffd888bad6bd2f8f9b6240
Xian Zheng <xzheng2112@gmail.com>
2018-02-23 18:11:44 -0700

    Labs 2 and 3

--- Labs/Objective1Lab2.java
+++ Labs/Objective1Lab2.java
@@ -2,5 +2,5 @@ public class Objective1Lab2{
   public static void main(String[] args) {
     System.out.println( "Xian Zheng" );
     System.out.println( "24" );
-}
+  }
 }
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,16 @@
+public class Objective1Lab3{
+  public static void main(String[] args) {
+    String fullName, state;
+    int age;
+
+    fullName = "Xian Zheng";
+    state = "Colorado";
+
+    age = 24;
+
+
+    System.out.println( "Name : " + fullName );
+    System.out.println( "Age : " + age );
+    System.out.println( "State : " + state );
+  }
+}
